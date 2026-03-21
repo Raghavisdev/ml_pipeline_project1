0e0c21b04f31ebe555432cf991d9adf6f57f6ce0
Raghavisdev <raghavacenger007@gmail.com>
2026-03-21 12:44:02 +0530

    Updated DataTransformation.

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,12 +5,12 @@ from src.logger import logging
 from src.exception import CustomException
 from dataclasses import dataclass
 from sklearn.model_selection import train_test_split
+from src.components.data_transformation import DataTransformation
 
-@dataclass
 class DataIngestionConfig:
-    train_data_path = os.path.join("artifacts", "train.csv")
-    test_data_path = os.path.join("artifacts", "test.csv")
-    raw_data_path = os.path.join("artifacts", "raw.csv")
+    train_data_path = os.path.join("artifacts/data_ingestion", "train.csv")
+    test_data_path = os.path.join("artifacts/data_ingestion", "test.csv")
+    raw_data_path = os.path.join("artifacts/data_ingestion", "raw.csv")
 
 class DataIngestion:
     def __init__(self):
@@ -20,6 +20,9 @@ class DataIngestion:
          try:
               logging.info("Data ingestion started")
               data = pd.read_csv(os.path.join("notebook", "online_shoppers_intention.csv"))
+
+              #removing duplicates 
+              data = data.drop_duplicates()
 
               os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
               data.to_csv(self.ingestion_config.raw_data_path, index=False)
@@ -41,4 +44,8 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data_path , test_data_path = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    train_arr , test_arr, _ = data_transformation.initiate_data_transformation(train_data_path , test_data_path)
+    
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -8,61 +8,87 @@ from sklearn.impute import SimpleImputer
 from dataclasses import dataclass
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
+from src.utils import save_obj
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path = os.path.join("artifacts/data_transformation" , "preprocessor.pkl")
+    preprocessor_obj_file_path = os.path.join("artifacts/data_transformation", "preprocessor.pkl")
 
 
 class DataTransformation:
     def __init__(self):
-        self.data_transformation_obj = DataTransformationConfig()
+        self.data_transformation_config = DataTransformationConfig()
 
     def get_data_transformation_obj(self):
-        # Encoded , Scaled
         try:
-            logging.info(" Data Tranformation Started ")
-
             num_cols = [
-            'Administrative', 'Administrative_Duration',
-            'Informational', 'Informational_Duration',
-            'ProductRelated', 'ProductRelated_Duration',
-            'BounceRates', 'ExitRates', 'PageValues', 'SpecialDay'
+                'Administrative','Administrative_Duration',
+                'Informational','Informational_Duration',
+                'ProductRelated','ProductRelated_Duration',
+                'BounceRates','ExitRates','PageValues','SpecialDay'
             ]
 
-            cat_cols = ['Month', 'VisitorType']
+            cat_cols = ['Month','VisitorType']
+            coded_cat_cols = ['OperatingSystems','Browser','Region','TrafficType']
+            bool_cols = ['Weekend']
 
-            coded_cat_cols = ['OperatingSystems', 'Browser', 'Region', 'TrafficType']
+            num_pipeline = Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(strategy="median")),
+                    ("scaler", StandardScaler())
+                ]
+            )
 
-            bool_cols = ['Weekend', 'Revenue']
+            cat_pipeline = Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(strategy="most_frequent")),
+                    ("onehot", OneHotEncoder(handle_unknown="ignore", sparse_output=False))
+                ]
+            )
 
             preprocessor = ColumnTransformer(
                 transformers=[
-                    ("num",StandardScaler(), num_cols),
-                    ("cat",OneHotEncoder(handle_unknown="ignore"),cat_cols),
-                    ("bool","passthrough", bool_cols)
+                    ("num", num_pipeline, num_cols),
+                    ("cat", cat_pipeline, cat_cols + coded_cat_cols),
+                    ("bool", "passthrough", bool_cols)
                 ]
-
             )
-            logging.info("Encoded and Scaled Successfully")
-
+            logging.info(" Pipeline Created ")
             return preprocessor
 
         except Exception as e:
+            raise CustomException(e, sys)
 
-            raise CustomException
-        
-
-    def inititiate_data_transformation(self,train_path,test_path):
+    def initiate_data_transformation(self, train_path, test_path):
         try:
             train_data = pd.read_csv(train_path)
             test_data = pd.read_csv(test_path)
 
-            
+            preprocessor_obj = self.get_data_transformation_obj()
+
+            target_column = "Revenue"
+
+            input_feature_train_data = train_data.drop(columns=[target_column])
+            target_feature_train_data = train_data[target_column]
+
+            input_feature_test_data = test_data.drop(columns=[target_column])
+            target_feature_test_data = test_data[target_column]
+
+            input_train_arr = preprocessor_obj.fit_transform(input_feature_train_data)
+            input_test_arr = preprocessor_obj.transform(input_feature_test_data)
+
+            train_arr = np.c_[input_train_arr, target_feature_train_data.to_numpy()]
+            test_arr = np.c_[input_test_arr, target_feature_test_data.to_numpy()]
+
+            save_obj(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessor_obj
+            )
+
+            print("Preprocessor saved")
+            logging.info("Preprocessor Saved")
+
+            return train_arr, test_arr, self.data_transformation_config.preprocessor_obj_file_path
+
         except Exception as e:
-
-            raise CustomException
-
-
-
-
+            raise CustomException(e, sys)
